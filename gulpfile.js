6d6fd32d5b6523d7c244d7ef202402b34bd088fd
Igor Ilchenko <wog516@gmail.com>
2016-07-27 14:28:36 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var del = require('del');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpif = require('gulp-if');
+var minifyCss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+
+gulp.task('clean', function () {
+    del('dist');
+});
+
+
+gulp.task('bundle', function () {
+    return gulp.src('www/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css', function () {
+    return gulp.src('www/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(concat('styles.min.css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build', ['clean', 'css', 'bundle'], function () {
+    console.log('build complete');
+});
